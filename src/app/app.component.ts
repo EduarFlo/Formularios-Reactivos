87b284ae69d15f4d2c720ed9192a867cac63ff38
Eduardo Gonzalez <eduardo.gonzalez.flores28@gmail.com>
2021-06-22 01:16:14 -0500

    Formularios Reactivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +7,33 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'forms';
+export class AppComponent implements OnInit {
+  actividad = new FormControl();
+
+  lista: string[] = [];
+
+  ngOnInit(): void {
+    this.lista=[];
+    let arreglo = JSON.parse(localStorage.getItem('actividades') || '{}');
+    if (arreglo!=null)
+      for(let actividad of arreglo)
+        this.lista.push(actividad);
+  }
+
+  agregar() {
+    this.lista.push(this.actividad.value);    
+    localStorage.setItem('actividades', JSON.stringify(this.lista));
+    this.actividad.setValue('');
+  }
+
+  borrar(pos:number) {
+    this.lista.splice(pos,1);
+    localStorage.clear();
+    localStorage.setItem('actividades', JSON.stringify(this.lista));
+  }
+
+  borrarTodas() {
+    localStorage.clear();
+    this.lista=[];
+  }
 }
